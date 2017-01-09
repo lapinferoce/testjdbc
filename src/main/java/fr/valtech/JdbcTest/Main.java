c55beed66e29d5c84268427eed81b8a7fbc1b124
lapinferoce <buk.rogers@gmail.com>
2017-01-09 09:56:02 +0000

    initial import

---
+++ src/main/java/fr/valtech/JdbcTest/Main.java
@@ -0,0 +1,13 @@
+package fr.valtech.JdbcTest;
+
+import java.awt.peer.SystemTrayPeer;
+
+/**
+ * Created by developer on 1/9/17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(" Posgresql connexion test ");
+
+    }
+}
